21eee1df12d6642ca5abaa2ad05a3b42c74948e0
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-02-09 09:29:59 +0100

    rtyklhjo4iwyhuyhuyhuyhuyhuyhuyhuyhuyhuyhuyhu6´p05hju5hju5hju5hju7ot9ìwyqhùyqhùyqhùyqhùyqhùyqhùyqhùyqhù j4ooooty9g0`54

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+let mi_jugadinha = 0
+input.onGesture(Gesture.Shake, function () {
+    mi_jugadinha = randint(1, 3)
+    if (mi_jugadinha == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            # # # # #
+            . # # # .
+            `)
+    } else if (mi_jugadinha == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (false) {
+    	
+    }
+})
 basic.forever(function () {
 	
 })
